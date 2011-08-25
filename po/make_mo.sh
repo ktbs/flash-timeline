87796be4961aa5925f97e5c0f7b4fb28b70869a6
jpichard <jpichard@9c2f2d7f-40d4-427b-a0ec-5f637d75cd7b>
2011-08-25 09:56:48 +0000

    Localisation des textes
    git-svn-id: svn://velvet.eu.com:3690/Timeline@84 9c2f2d7f-40d4-427b-a0ec-5f637d75cd7b

--- po/make_mo.sh
+++ po/make_mo.sh
@@ -12,5 +12,5 @@ mkdir -p $out
 for locale in $locales; do
     echo "Processing "$locale".po ..."
     mkdir -p $out/$locale/LC_MESSAGES
-    "/cygdrive/c/Program Files/GnuWin32/bin/msgfmt" --check --verbose --output-file $out/$locale/LC_MESSAGES/timeline.mo $locale.po
+    msgfmt --check --verbose --output-file $out/$locale/LC_MESSAGES/timeline.mo $locale.po
 done
