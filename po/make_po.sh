1a42c490b61ee7773923bec23f19bef8a843f860
jpichard <jpichard@9c2f2d7f-40d4-427b-a0ec-5f637d75cd7b>
2011-09-05 08:22:44 +0000

    Ajout de textes localisés (Ok / Annuler / Écrivez votre commentaire texte sélectionné qd on ouvre la fenêtre des commentaires gestion des touches entrée pour Ok et Annuler quand les boutons ont le focus
    git-svn-id: svn://velvet.eu.com:3690/Timeline@125 9c2f2d7f-40d4-427b-a0ec-5f637d75cd7b

--- po/make_po.sh
+++ po/make_po.sh
@@ -1,7 +1,7 @@
 # Si on utilise un translator, utiliser ce script, sinon, recopier à partir du template les nouvelles entrées dans les fichiers po
 #!/bin/sh
 
-pot=`find . | grep -i \.pot$`
+#pot=`find . | grep -i \.pot$`
 
 #msginit --no-translator --locale fr_FR --output-file fr_FR.po --input $pot
 #msginit --no-translator --locale fr_FR --output-file en_US.po --input $pot
